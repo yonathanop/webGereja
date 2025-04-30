efb391012ec66a03cba28063cd36cfd283a396cb
yonathanop <yonathanpradana099@gmail.com>
2025-04-30 08:02:40 +0700

    memperbaiki navbar, menambah carousel pada hero section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,13 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
     <title>Home | GPdI House of Worship</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg "style="background-color: #0a3d62;">
+    <nav class="navbar navbar-expand-lg navbar-dark"style="background-color: #0a3d62;">
       <div class="container-fluid">
         <a class="navbar-brand text-white d-flex align items-center" href="#">
           {{-- Logo --}}
@@ -23,8 +23,8 @@
         </button>
 
         {{-- Menu Navbar --}}
-        <div class="collapse navbar-collapse d-flex justify-content-center w-100" id="navbarNav">
-          <ul class="navbar-nav flex-row mx-auto" style="transform: translateX(50px);">
+        <div class="collapse navbar-collapse justify-content-lg-center w-100" id="navbarNav">
+          <ul class="navbar-nav mx-auto text-start text-lg-center">
             <li class="nav-item px-3">
               <a class="nav-link active text-white" aria-current="page" href="/home">Home</a>
             </li>
@@ -41,11 +41,20 @@
               <a class="nav-link active text-white" aria-current="page" href="/kontak">Kontak</a>
             </li>
           </ul>
-          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <!-- Login untuk Mobile (masuk ke dalam menu utama) -->
+          <ul class="navbar-nav d-lg-none ps-3">
             <li class="nav-item">
-              <a class="nav-link active text-white" aria-current="page" href="/login">Login</a>
+              <a class="nav-link active text-white" href="/login">Login</a>
             </li>
           </ul>
+
+          <!-- Login untuk Desktop (posisi kanan) -->
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0 d-none d-lg-flex">
+            <li class="nav-item">
+              <a class="nav-link active text-white" href="/login">Login</a>
+            </li>
+          </ul>
+
         </div>
       </div>
     </nav>
@@ -63,6 +72,25 @@
    </section>
    <section id="hero1" class="py-5" style="background-image: url('{{ asset('img/hero1.png') }}');">
        </section>
+       <section id="heroCarousel" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner text-center">
+          <div class="carousel-item active">
+            <img src="{{ asset('img/logoGPdI.jpg') }}" class="d-block mx-auto" style="max-height: 300px;" alt="slide 1">
+          </div>
+          <div class="carousel-item active">
+            <img src="{{ asset('img/hero1.png') }}" class="d-block mx-auto" style="max-height: 300px;" alt="slide 2">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#heroCarousel" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#heroCarousel" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </section>
+      
    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
   </body>
